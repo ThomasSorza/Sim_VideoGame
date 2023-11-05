de363c7cdbd107f06940e6dc7d4a1421b6add542
Thomas Sorza <76008678+ThomasSorza@users.noreply.github.com>
2023-11-05 14:16:49 -0500

    added enemy from 1st level

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterDamage : MonoBehaviour
+public class EnemyDamage : MonoBehaviour
 {
     public int damage;
     public PlayerHealth playerHealth;
 
+    void Start()
+    {
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
             playerHealth.TakeDamage(damage);
         }
