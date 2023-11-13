aea62f1eb99ea27dc736de04be7bef03162058d0
Esteban Barrero <barrerodavid10@gmail.com>
2023-11-13 11:15:19 -0500

    Commit 003, soundtrack

--- Assets/Fruit_Collected.cs
+++ Assets/Fruit_Collected.cs
@@ -14,14 +14,14 @@ public class Fruit_Collected : MonoBehaviour
             GetComponent<SpriteRenderer>().enabled = false;
 
             // Verifica que hay al menos dos hijos antes de intentar acceder al segundo hijo.
-            if (transform.childCount >= 0)
+            /*if (transform.childCount >= 0)
             {
                 transform.GetChild().gameObject.SetActive(true);
             }
             else
             {
                 Debug.LogWarning("No hay suficientes hijos en el objeto actual.");
-            }
+            }*/
 
             Destroy(gameObject, 0.5f);
             clip.Play();
